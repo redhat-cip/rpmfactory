01a8f06c9e444a0949a675ac528b6b4c79d0b783
Fabien Boucher <fabien.boucher@enovance.com>
2015-11-26 17:58:54 +0100

    Add koji-build.sh a script to build and fetch artifacts (rpms)
    Change-Id: Ifa3d2605585de258a49fcc3c3b234f0e5d418913

---
+++ koji-build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+srpm=$1
+dist=dist-centos7
+koji_internal_path=/mnt/koji/work/tasks/
+koji_tasks_path=http://46.231.133.39/kojifiles/work/tasks/
+
+echo "Start build of: $1"
+set +e
+koji build --scratch $dist $srpm &> /tmp/out
+set -e
+tid=$(grep 'Created' /tmp/out | awk -F': ' '{print $2}')
+echo "Task id is: $tid"
+while true; do
+    koji taskinfo -vr $tid &> /tmp/out2
+    state=$(egrep "^State:" /tmp/out2 | awk -F': ' '{print $2}')
+    if [ "$state" = "closed" -o "$state" = "failed" ]; then
+        echo "Task $tid finished with status: $state"
+        break
+    else
+        echo "Task $tid is processing: $state ..."
+        sleep 5
+    fi
+done
+if [ "$state" = "failed" ]; then
+    echo "Task $tid failed. exit 1."
+    exit 1
+fi
+if [ "$state" = "closed" ]; then
+    echo "Task $tid succeed. Fetching rpms ..."
+    pkgs=$(egrep rpm$ /tmp/out2 | egrep -v src.rpm$ | sed "s|.*$koji_internal_path||")
+    echo $pkgs
+    for pkg in "$pkgs"; do
+        echo "Fetching rpms $(basename $pkg) ..."
+        curl $koji_tasks_path/$pkg -o $(basename $pkg)
+    done
+fi
