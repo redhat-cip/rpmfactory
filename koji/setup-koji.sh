e6228eb0879634405214fc39dd7ac8c124a9a573
Sebastien Badia <sbadia@redhat.com>
2015-11-27 14:56:43 +0100

    koji: Add a setup script for post-install operations
    Configure builders/tags/targets and repos.
    
    Change-Id: Ic1ef0ae015cf977328a9234a380f49149a621883

---
+++ koji/setup-koji.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Setup Koji builders/Repos/Targets
+
+for i in $(seq 1 2);
+do
+  koji add-host "koji-build0${i}" x86_64
+  koji add-host-to-channel "koji-build0${i}" createrepo
+  koji edit-host --capacity=6 "koji-build0${i}"
+done
+
+koji add-tag dist-centos7
+koji add-tag --parent dist-centos7 --arches "x86_64" dist-centos7-build
+koji add-external-repo -t dist-centos7-build dist-centos7-repo http://ftp.free.fr/mirrors/ftp.centos.org/7/os/\$arch/
+koji add-external-repo -t dist-centos7-build dist-centos7-repo-updates http://ftp.free.fr/mirrors/ftp.centos.org/7/updates/\$arch/
+koji add-target dist-centos7 dist-centos7-build
+koji add-group-pkg dist-centos7-build build bash bzip2 coreutils cpio diffutils findutils gawk gcc grep sed gcc-c++ gzip info patch redhat-rpm-config rpm-build shadow-utils tar unzip util-linux-ng which make
+koji add-group-pkg dist-centos7-build srpm-build bash gnupg make redhat-rpm-config rpm-build shadow-utils wget rpmdevtools
+koji regen-repo dist-centos7-build
