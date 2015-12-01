08fb280f41ef5566c42e8f248700d2d74f70383c
Sebastien Badia <sbadia@redhat.com>
2015-12-01 16:34:02 +0100

    nodepool: Sync koji setup script with RPM Factory
    http://rpmfactory.beta.rdoproject.org/r/#/c/21/
    
    Change-Id: I7ee0199488f749fba55486bf76ade97af7bf0758

--- nodepool/koji_slave_setup.sh
+++ nodepool/koji_slave_setup.sh
@@ -2,20 +2,34 @@
 
 . base.sh
 
-sudo yum install -y fedora-packager wget
+sudo yum install -y koji wget python-pip
+sudo pip install -U tox==1.6.1
 
 sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
 sudo chmod 600 /srv/swap
 sudo mkswap /srv/swap
 grep swap /etc/fstab || echo "/srv/swap none swap sw 0 0" | sudo tee -a /etc/fstab
 
-sudo sed -i 's/^.*SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
-
 # Koji configuration and certs (insecure)
 sudo wget http://192.168.42.27/conf/koji.conf -O /etc/koji.conf
 sudo wget http://192.168.42.27/conf/jenkins-koji.tgz -O /srv/jenkins-koji.tgz
 sudo tar xvzf /srv/jenkins-koji.tgz -C /home/jenkins/
 sudo chown -R jenkins /home/jenkins/.koji
+
+# install ansible
+sudo yum install -y epel-release
+sudo yum install -y python-crypto git
+sudo pip install -U ansible==1.9.2
+ansible --version
+sudo yum remove -y epel-release
+
+# install khaleesi
+git clone https://github.com/redhat-openstack/khaleesi.git
+git clone https://github.com/redhat-openstack/khaleesi-settings.git
+
+pushd khaleesi/tools/ksgen
+sudo python setup.py install
+popd
 
 # sync FS, otherwise there are 0-byte sized files from the yum/pip installations
 sudo sync
