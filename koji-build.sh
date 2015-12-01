1dc92729f093d9463c3b94a3d18cae84a3e35571
Sebastien Badia <sbadia@redhat.com>
2015-12-01 18:03:10 +0100

    jenkins: Fix koji-build script (quoted iterator)
    Since $pkgs is double quoted, it will not word split, and the loop will
    only run once.
    
    Change-Id: I73339aa01470dbf2f92a03b6a3318f55341a0ee5

--- koji-build.sh
+++ koji-build.sh
@@ -3,20 +3,20 @@
 set -e
 
 srpm=$1
-dist=dist-centos7
+dist='dist-centos7'
 koji_internal_path=/mnt/koji/work/tasks/
-koji_tasks_path=http://46.231.133.39/kojifiles/work/tasks/
-koji_ui_tasks_uri=http://koji-ctrl.ring.enovance.com/koji/taskinfo?taskID=
+koji_tasks_path='http://koji-ctrl.ring.enovance.com/kojifiles/work/tasks/'
+koji_ui_tasks_uri='http://koji-ctrl.ring.enovance.com/koji/taskinfo?taskID='
 
 echo "Start build of: $1"
 set +e
-koji build --scratch $dist $srpm &> /tmp/out
+koji build --scratch "$dist" "$srpm" &> /tmp/out
 set -e
 tid=$(grep 'Created' /tmp/out | awk -F': ' '{print $2}')
 echo "Task id is: $tid"
 echo "Task console is: ${koji_ui_tasks_uri}${tid}"
 while true; do
-    koji taskinfo -vr $tid &> /tmp/out2
+    koji taskinfo -vr "$tid" &> /tmp/out2
     state=$(egrep "^State:" /tmp/out2 | awk -F': ' '{print $2}')
     if [ "$state" = "closed" -o "$state" = "failed" ]; then
         echo "Task $tid finished with status: $state"
@@ -33,9 +33,9 @@ fi
 if [ "$state" = "closed" ]; then
     echo "Task $tid succeed. Fetching rpms ..."
     pkgs=$(egrep rpm$ /tmp/out2 | egrep -v src.rpm$ | sed "s|.*$koji_internal_path||")
-    echo $pkgs
-    for pkg in "$pkgs"; do
-        echo "Fetching rpms $(basename $pkg) ..."
-        curl $koji_tasks_path/$pkg -o $(basename $pkg)
+    echo "$pkgs"
+    for pkg in $pkgs; do
+        echo "Fetching rpms $(basename "$pkg") ..."
+        curl "$koji_tasks_path/$pkg" -o $(basename "$pkg")
     done
 fi
