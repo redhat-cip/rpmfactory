449629a46a061f5b47d82a33381433050fbe0bfe
Sebastien Badia <sbadia@redhat.com>
2016-01-13 16:39:42 +0100

    zuul: A dirty script in order to generate rpmfactory zuul configuration
    Change-Id: Ie3649ba7998bde3b645cdd38cce0850f89175727

---
+++ zuul/generate-zuul-rpmfactory.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Copyright (C) 2015 RedHat <softwarefactory@redhat.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2 of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+if [ "x${DEBUG}" == "x1" ]; then
+  set -x
+fi
+
+rpmfactory_uri='rpmfactory.beta.rdoproject.org'
+rpmfactory_user=$USER
+
+GIT_RPMFACT=$(
+  export LC_ALL=C
+  ssh "$rpmfactory_uri" -l "$rpmfactory_user" -p 29418 \
+    gerrit ls-projects \
+      | sort \
+      | sed '/config/d;/All-Users/d;s/\.git$//'
+)
+
+echo '# This file get appended to layout.yaml, thus the lack of'
+echo "projects:"
+for git in $GIT_RPMFACT;
+do
+    echo "  - name: ${git}"
+    echo "    check:"
+    echo "      - tox-validate"
+    echo "    periodic:"
+    echo "      - upstream-update"
+  if [[ $git =~ -dist ]]; then
+    echo "  - name: ${git}"
+    echo "    check:"
+    echo "      - pkg-validate"
+    echo "      - delorean-ci"
+  fi
+echo ''
+done # for git in $GIT_RPMFACT
+
+# sync-upstream.sh ends here
